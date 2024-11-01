c673bd0a7091de585094d8707ce3c434814d6c1e
tanioic <tanimoto@oic.jp>
2024-11-01 09:30:52 +0900

    ボタンを追加、GitHubラベルの位置調整

--- WinFormsAppUsingGitHub/Form1.Designer.cs
+++ WinFormsAppUsingGitHub/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI", 72F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(21, 28);
             label1.Name = "label1";
             label1.Size = new Size(354, 128);
             label1.TabIndex = 0;
             label1.Text = "GitHub";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 72F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(381, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(361, 160);
+            button1.TabIndex = 1;
+            button1.Text = "Button";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -56,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
